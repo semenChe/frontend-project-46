88bc3beb52e3c4cf77fa91b69e7e3689b001a519
Semen Cernyshev <semen_ch@vk.com>
2022-10-26 15:30:24 +0300

    modified test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,19 @@
 import genDiff from '../src/index.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('genDiff', () => {
-    const patch1 = '__fixtures__/file1.json';
-    const patch2 = '__fixtures__/file2.json';
+    const patch1 = 'file1.json';
+    const patch2 = 'file2.json';
+
+
+
     const result1 = '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
-    expect(genDiff(patch1, patch2)).toEqual(result1);
+    expect(genDiff(getFixturePath(patch1), getFixturePath(patch2))).toEqual(result1);
 });
