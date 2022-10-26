6e1272c35db5fb5a94d749ce8e92bf1db8efb929
Semen Cernyshev <semen_ch@vk.com>
2022-10-26 11:45:54 +0300

    modified index.test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,8 @@
 import genDiff from '../src/index.js';
 
 test('genDiff', () => {
-    const patch1 = '/Users/semencernysev/Public/frontend-project-46/__fixtures__/file1.json';
-    const patch2 = '/Users/semencernysev/Public/frontend-project-46/__fixtures__/file2.json';
+    const patch1 = '__fixtures__/file1.json';
+    const patch2 = '__fixtures__/file2.json';
     const result1 = '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
     expect(genDiff(patch1, patch2)).toEqual(result1);
 });
