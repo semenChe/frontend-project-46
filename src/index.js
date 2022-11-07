4d221a40e165270bbfb1c8ed58bd2174f854ec23
Semen Cernyshev <semen_ch@vk.com>
2022-11-07 11:41:12 +0300

    fix Unallowed reassignment

--- src/index.js
+++ src/index.js
@@ -14,15 +14,21 @@ export default (path1, path2, formatName = 'stylish') => {
     return keys.reduce((acc, key) => {
       if (!Object.hasOwn(data1, key)) {
         acc[key] = { difference: 'added', value: data2[key] }; // ключ отсутствовал в первом объекте, но был добавлен во второй
-      } else if (!Object.hasOwn(data2, key)) {
-        acc[key] = { difference: 'deleted', value: data1[key] }; // ключ был в первом объекте, но отсутствует во втором
-      } else if (data1[key] !== data2[key] && _.isObject(data1[key]) && _.isObject(data2[key])) {
-        acc[key] = { difference: 'changed with children', value: genDiff(data1[key], data2[key]) }; // оба значения объекты
-      } else if (data1[key] !== data2[key]) {
-        acc[key] = { difference: 'changed', value1: data1[key], value2: data2[key] }; // ключ присутствовал и в первом и во втором объектах, но значения отличаются
-      } else {
-        acc[key] = { difference: 'unchanged', value: data1[key] }; // ключ присутствовал и в первом и во втором объектах с одинаковыми значениями
+        return acc;
       }
+      if (!Object.hasOwn(data2, key)) {
+        acc[key] = { difference: 'deleted', value: data1[key] }; // ключ был в первом объекте, но отсутствует во втором
+        return acc;
+      }
+      if (data1[key] !== data2[key] && _.isObject(data1[key]) && _.isObject(data2[key])) {
+        acc[key] = { difference: 'changed with children', value: genDiff(data1[key], data2[key]) }; // оба значения объекты
+        return acc;
+      }
+      if (data1[key] !== data2[key]) {
+        acc[key] = { difference: 'changed', value1: data1[key], value2: data2[key] }; // ключ присутствовал и в первом и во втором объектах, но значения отличаются
+        return acc;
+      }
+      acc[key] = { difference: 'unchanged', value: data1[key] }; // ключ присутствовал и в первом и во втором объектах с одинаковыми значениями
       return acc;
     }, {});
   };
