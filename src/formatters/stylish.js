71b72973bfcbb51eeb9f9a01830895cfa86cfa76
Semen Cernyshev <semen_ch@vk.com>
2022-11-08 12:03:28 +0300

    cognitive complexity refactoring 3

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -9,12 +9,20 @@ export default (diff) => {
     const currentIndent = replacer.repeat(indentSize);
     const bracketIndent = replacer.repeat(indentSize - spacesCount);
     const lines = Object.entries(currentValue).map(([key, val]) => {
-      if (val.difference === 'nested') return `${currentIndent}  ${key}: ${iter(val.value, depth + 2)}`;
-      if (val.difference === 'changed') return `${currentIndent}- ${key}: ${iter(val.value1, depth + 2)}\n${currentIndent}+ ${key}: ${iter(val.value2, depth + 2)}`;
-      if (val.difference === 'added') return `${currentIndent}+ ${key}: ${iter(val.value, depth + 2)}`;
-      if (val.difference === 'deleted') return `${currentIndent}- ${key}: ${iter(val.value, depth + 2)}`;
-      if (val.difference === 'unchanged') return `${currentIndent}  ${key}: ${val.value}`;
-      return (_.isObject(val)) ? `${currentIndent}  ${key}: ${iter(val, depth + 2)}` : `${currentIndent}  ${key}: ${val}`;
+      switch (val.difference) {
+        case 'nested':
+          return `${currentIndent}  ${key}: ${iter(val.value, depth + 2)}`;
+        case 'changed':
+          return `${currentIndent}- ${key}: ${iter(val.value1, depth + 2)}\n${currentIndent}+ ${key}: ${iter(val.value2, depth + 2)}`;
+        case 'added':
+          return `${currentIndent}+ ${key}: ${iter(val.value, depth + 2)}`;
+        case 'deleted':
+          return `${currentIndent}- ${key}: ${iter(val.value, depth + 2)}`;
+        case 'unchanged':
+          return `${currentIndent}  ${key}: ${val.value}`;
+        default:
+          return (_.isObject(val)) ? `${currentIndent}  ${key}: ${iter(val, depth + 2)}` : `${currentIndent}  ${key}: ${val}`;
+      }
     });
     return ['{', ...lines, `${bracketIndent}}`].join('\n');
   };
