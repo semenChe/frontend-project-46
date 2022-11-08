7dc293c5246d9621c41a705e2022015dc9e07ffc
Semen Cernyshev <semen_ch@vk.com>
2022-11-08 09:22:41 +0300

    modified f iter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -5,40 +5,30 @@ export default (diff) => {
     if (!_.isObject(currentValue)) {
       return `${currentValue}`;
     }
-
-    const replacer = ' ';
     const spacesCount = 2;
-    const indentSize = depth * spacesCount;
-    const currentIndent = replacer.repeat(indentSize);
-    const bracketIndent = replacer.repeat(indentSize - spacesCount);
-
-    const lines = Object.entries(currentValue)
-      .map(([key, val]) => {
-        if (val.difference === 'nested') {
-          return `${currentIndent}  ${key}: ${iter(val.value, depth + 2)}`;
-        }
-        if (val.difference === 'changed') {
-          return `${currentIndent}- ${key}: ${iter(val.value1, depth + 2)}\n${currentIndent}+ ${key}: ${iter(val.value2, depth + 2)}`;
-        }
-        if (val.difference === 'added') {
-          return `${currentIndent}+ ${key}: ${iter(val.value, depth + 2)}`;
-        }
-        if (val.difference === 'deleted') {
-          return `${currentIndent}- ${key}: ${iter(val.value, depth + 2)}`;
-        }
-        if (val.difference === 'unchanged') {
-          return `${currentIndent}  ${key}: ${val.value}`;
-        }
-        if (_.isObject(val)) {
-          return `${currentIndent}  ${key}: ${iter(val, depth + 2)}`;
-        }
-        return `${currentIndent}  ${key}: ${val}`;
-      });
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
+    const currentIndent = ' '.repeat(depth * spacesCount);
+    const lines = Object.entries(currentValue).map(([key, val]) => {
+      if (val.difference === 'nested') {
+        return `${currentIndent}  ${key}: ${iter(val.value, depth + 2)}`;
+      }
+      if (val.difference === 'changed') {
+        return `${currentIndent}- ${key}: ${iter(val.value1, depth + 2)}\n${currentIndent}+ ${key}: ${iter(val.value2, depth + 2)}`;
+      }
+      if (val.difference === 'added') {
+        return `${currentIndent}+ ${key}: ${iter(val.value, depth + 2)}`;
+      }
+      if (val.difference === 'deleted') {
+        return `${currentIndent}- ${key}: ${iter(val.value, depth + 2)}`;
+      }
+      if (val.difference === 'unchanged') {
+        return `${currentIndent}  ${key}: ${val.value}`;
+      }
+      if (_.isObject(val)) {
+        return `${currentIndent}  ${key}: ${iter(val, depth + 2)}`;
+      }
+      return `${currentIndent}  ${key}: ${val}`;
+    });
+    return ['{', ...lines, `${' '.repeat(depth * spacesCount - spacesCount)}}`].join('\n');
   };
 
   return iter(diff, 1);
