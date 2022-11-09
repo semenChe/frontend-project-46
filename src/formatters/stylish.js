3bef59133f2c343236b3e4033294f33af9825768
Semen Cernyshev <semen_ch@vk.com>
2022-11-09 22:29:53 +0300

    making edits 2.1

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -9,17 +9,17 @@ export default (diff) => {
     const indentSize = depth * spacesCount;
     const currentIndent = replacer.repeat(indentSize);
     const bracketIndent = replacer.repeat(indentSize - spacesCount);
-    const stringFormation = (operator, key, val) => `${currentIndent}${operator} ${key}: ${iter(val, depth + 2)}`;
+    const stringGener = (operator, key, val) => `${currentIndent}${operator} ${key}: ${iter(val, depth + 2)}`;
     const lines = Object.entries(currentValue).map(([key, val]) => {
       switch (val.difference) {
         case 'changed':
-          return `${stringFormation('-', key, val.value1)}\n${stringFormation('+', key, val.value2)}`;
+          return `${stringGener('-', key, val.value1)}\n${stringGener('+', key, val.value2)}`;
         case 'added':
-          return stringFormation('+', key, val.value);
+          return stringGener('+', key, val.value);
         case 'deleted':
-          return stringFormation('-', key, val.value);
+          return stringGener('-', key, val.value);
         default:
-          return stringFormation(' ', key, val.value || val, depth);
+          return stringGener(' ', key, val.value || val, depth);
       }
     });
     return ['{', ...lines, `${bracketIndent}}`].join('\n');
