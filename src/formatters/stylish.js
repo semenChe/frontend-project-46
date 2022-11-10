369bd29b1147b7edfc706fc0081a351a6ceca83e
Semen Cernyshev <semen_ch@vk.com>
2022-11-10 14:23:20 +0300

    making edits 3.4

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -9,17 +9,17 @@ export default (diff) => {
     const indentSize = depth * spacesCount;
     const currentIndent = replacer.repeat(indentSize);
     const bracketIndent = replacer.repeat(indentSize - spacesCount);
-    const stringGener = (operator, key, val) => `${currentIndent}${operator} ${key}: ${iter(val, depth + 2)}`;
+
     const lines = Object.entries(currentValue).map(([key, val]) => {
       switch (val.difference) {
         case 'changed':
-          return `${stringGener('-', key, val.value1)}\n${stringGener('+', key, val.value2)}`;
+          return `${currentIndent}- ${key}: ${iter(val.value1, depth + 2)}\n${currentIndent}+ ${key}: ${iter(val.value2, depth + 2)}`;
         case 'added':
-          return stringGener('+', key, val.value);
+          return `${currentIndent}+ ${key}: ${iter(val.value, depth + 2)}`;
         case 'deleted':
-          return stringGener('-', key, val.value);
+          return `${currentIndent}- ${key}: ${iter(val.value, depth + 2)}`;
         default:
-          return stringGener(' ', key, val.value || val, depth);
+          return `${currentIndent}  ${key}: ${iter(val.value || val, depth + 2)}`;
       }
     });
     return ['{', ...lines, `${bracketIndent}}`].join('\n');
