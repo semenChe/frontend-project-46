8e47db13867c86298ce15f2d8b22c1681b200380
Semen Cernyshev <semen_ch@vk.com>
2022-11-10 14:33:38 +0300

    making edits 3.5

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,27 +3,30 @@ import _ from 'lodash';
 const replacer = ' ';
 const spacesCount = 2;
 
-export default (diff) => {
+export default (value) => {
   const iter = (currentValue, depth) => {
     if (!_.isObject(currentValue)) return `${currentValue}`;
+
     const indentSize = depth * spacesCount;
     const currentIndent = replacer.repeat(indentSize);
     const bracketIndent = replacer.repeat(indentSize - spacesCount);
-
-    const lines = Object.entries(currentValue).map(([key, val]) => {
-      switch (val.difference) {
-        case 'changed':
-          return `${currentIndent}- ${key}: ${iter(val.value1, depth + 2)}\n${currentIndent}+ ${key}: ${iter(val.value2, depth + 2)}`;
-        case 'added':
-          return `${currentIndent}+ ${key}: ${iter(val.value, depth + 2)}`;
-        case 'deleted':
-          return `${currentIndent}- ${key}: ${iter(val.value, depth + 2)}`;
-        default:
-          return `${currentIndent}  ${key}: ${iter(val.value || val, depth + 2)}`;
-      }
-    });
+    const lines = Object
+      .entries(currentValue)
+      .map(([key, val]) => {
+        switch (val.difference) {
+          case 'changed':
+            return `${currentIndent}- ${key}: ${iter(val.value1, depth + 2)
+            }\n${currentIndent}+ ${key}: ${iter(val.value2, depth + 2)}`;
+          case 'added':
+            return `${currentIndent}+ ${key}: ${iter(val.value, depth + 2)}`;
+          case 'deleted':
+            return `${currentIndent}- ${key}: ${iter(val.value, depth + 2)}`;
+          default:
+            return `${currentIndent}  ${key}: ${iter(val.value || val, depth + 2)}`;
+        }
+      });
     return ['{', ...lines, `${bracketIndent}}`].join('\n');
   };
 
-  return iter(diff, 1);
+  return iter(value, 1);
 };
